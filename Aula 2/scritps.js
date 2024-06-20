b0c608ae080ebb1161c7c2ff7ccb15eceab7a00e
DiegoBorges26 <diego.fort2011@hotmail.com>
2024-06-20 11:49:51 -0300

    Add files via upload

---
+++ Aula 2/scritps.js
@@ -0,0 +1,56 @@
+//ATIVIDADE 1
+// var num = Number(prompt("Digite um numero:));
+
+// if( num % 2 == 0 && num > 0){
+//     console.log('O numero é positivo e par')
+// }
+
+// else{
+//     console.log(' Não é positivo e e nem par')
+// }
+
+//ATIVIDADE 2
+
+// var peso = Number(prompt("Digite seu peso:"));
+// var altura = Number(prompt("Digite sua altura:"));
+// var imc;
+
+// imc = peso/(altura*algura)
+
+// if( imc < 18,5){
+//     console.log("Abaixo do peso")
+// }
+
+// if( imc >= 18,5 && imc < 25){
+//     console.log("Peso normal")
+// }
+
+// if( imc >= 25 && imc < 30){
+//     console.log("Sobrepeso")
+// }
+
+// if( imc >= 30 && imc < 35) {
+//     console.log(" Obesidade Grau I")
+// }
+
+// if( imc >= 35 && imc < 40){
+//     console.log(" Obesidade Grau II")
+// }
+
+// else{
+//     console.log(" Morbita")
+// }
+
+
+//ATIVIDADE 3 ( não consegui)
+
+var = Number(prompt("Digite o valor do produto:")):
+var = Number(prompt("Digite sua idade"));
+
+if( idade < 18)
+    valor*10/100
+
+
+
+//ATIVIDADE 4 
+
